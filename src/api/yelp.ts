0ae6cead2373df6c31dc5803df36dafa4a6b47c2
benjamin_rowz <benjaminharringtonrose@gmail.com>
2020-11-15 08:37:28 -0500

    Clean up ignored files

--- src/api/yelp.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.yelp.com/v3/businesses",
-  headers: {
-    Authorization:
-      "Bearer wNV-UStyzRERSKkOo4dyGKXWDAnpUaKagh_ykGVWrEQjb-GYV6yy-7qyRZtc9DzHLcYE716vesFfCGqOVe3jY_DxndKdUN7iJ6lbKG2fMpe92hZhfzo7ikbSl0qsX3Yx",
-  },
-});
